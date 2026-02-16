98bfed8571d81507c94971448a5d1351a5e45788
Mauricio Rojas <mauriciorojas@MacBook-Pro-de-Mauricio-R.local>
2026-02-16 16:07:52 +0100

    images

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,13 @@
 import React from "react";
-
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
-import * as coreui from '@coreui/coreui';
-import '@coreui/coreui/dist/css/coreui.min.css';
-
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
